612fcb40435fbb453944c9bbe7d05651343bcc91
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2025-11-06 16:03:35 +0530

    lecture 2

--- recurssion/problem.py
+++ recurssion/problem.py
@@ -36,12 +36,52 @@ class LectureOne:
         LectureOne.print_n_to_1_bt(n-1)
 
 
+class LectureTwo:
+
+    @staticmethod
+    def sum_of_first_n_num(n):
+        # base cond.
+        if n == 0:
+            return 0
+
+        x = n + LectureTwo.sum_of_first_n_num(n-1)
+        return x
+
+    @staticmethod
+    def reverse_array(i, arr, n):
+        # base cond.
+        if i >= n/2:
+            return
+
+        arr[i], arr[n-i-1] = arr[n-i-1], arr[i]  # swap i & n-i-1
+        LectureTwo.reverse_array(i+1, arr, n)
+
+    @staticmethod
+    def string_palindrom(i, str, n):
+        # base cond.
+        if i >= n/2:
+            return True
+
+        if str[i] != str[n-i-1]:
+            return False
+
+        return LectureTwo.string_palindrom(i+1, str, n)
+
+
 def main():
-    LectureOne.print_name_n_times('Akash', 5)
-    LectureOne.print_1_to_n(1, 10)
-    LectureOne.print_n_to_1(10)
-    LectureOne.print_1_to_n_bt(3)
-    LectureOne.print_n_to_1_bt(3)
+    # LectureOne.print_name_n_times('Akash', 5)
+    # LectureOne.print_1_to_n(1, 10)
+    # LectureOne.print_n_to_1(10)
+    # LectureOne.print_1_to_n_bt(3)
+    # LectureOne.print_n_to_1_bt(3)
+
+    print(LectureTwo.sum_of_first_n_num(5))
+
+    arr = [1, 2, 3, 4]
+    LectureTwo.reverse_array(i=0, arr=arr, n=4)
+    print(arr)
+
+    print(LectureTwo.string_palindrom(i=0, str='madam', n=5))
 
 
 if __name__ == "__main__":
