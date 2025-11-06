4f68f92610c195b5faa04f7e017372cdeec3860e
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2025-11-06 08:43:21 +0530

    ./

--- recurssion/problem.py
+++ recurssion/problem.py
@@ -1,48 +1,47 @@
-# 1. print name N times using recursion
-def print_name_n_times(name, n):
-    if n <= 0:
-        return
-    print(f'{n} = {name}')
-    n -= 1
-    print_name_n_times(name=name, n=n)
 
-# 2. print linerly from 1 to n
-def print_1_to_n(i, n):
-    if i > n:
-        return
-    print(i)
-    i += 1
-    print_1_to_n(i=i, n=n)
+class LectureOne:
+    @staticmethod
+    def print_name_n_times(name, n):
+        if n <= 0:
+            return
+        print(f'{n} = {name}')
+        LectureOne.print_name_n_times(name=name, n=n-1)
 
-# 3. print from n to 1
-def print_n_to_1(n):
-    if n <= 0:
-        return
-    print(n)
-    n -= 1
-    print_n_to_1(n)
+    @staticmethod
+    def print_1_to_n(i, n):
+        if i > n:
+            return
+        print(i)
+        LectureOne.print_1_to_n(i=i+1, n=n)
 
-# 4. print 1 to n [backtraacking]
-def print_1_to_n_bt(i):
-    if i <= 1:
-        return
-    print_1_to_n_bt(i=i-1)
-    print(i)
+    @staticmethod
+    def print_n_to_1(n):
+        if n <= 0:
+            return
+        print(n)
+        LectureOne.print_n_to_1(n-1)
 
-# 5. print n to 1 [backtracking]
-def print_n_to_1_bt(n):
-    if n < 1:
-        return
-    print(n)
-    print_n_to_1_bt(n - 1)
+    @staticmethod
+    def print_1_to_n_bt(i):
+        if i <= 1:
+            return
+        LectureOne.print_1_to_n_bt(i-1)
+        print(i)
+
+    @staticmethod
+    def print_n_to_1_bt(n):
+        if n < 1:
+            return
+        print(n)
+        LectureOne.print_n_to_1_bt(n-1)
 
 
 def main():
-    print_name_n_times('Akash', 5)
-    print_1_to_n(1, 10)
-    print_n_to_1(10)
-    print_1_to_n_bt(3)
-    print_n_to_1_bt(3)
+    LectureOne.print_name_n_times('Akash', 5)
+    LectureOne.print_1_to_n(1, 10)
+    LectureOne.print_n_to_1(10)
+    LectureOne.print_1_to_n_bt(3)
+    LectureOne.print_n_to_1_bt(3)
 
 
 if __name__ == "__main__":
